c2f991027f85be99cc18e5f876c0c15914a17871
souf139 <soufianboushaba12@gmail.com>
2023-11-01 12:40:46 +0100

    this commit for creating  hero commponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <AppHeader />
+    <AppHero />
   </div>
 </template>
 
 <script>
 import AppHeader from "@/components/AppHeader.vue";
-
+import AppHero from "@/components/AppHero.vue";
 export default {
   components: {
     AppHeader,
+    AppHero,
+
   },
 };
 </script>
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -68,7 +68,7 @@
 
 <script>
 export default {
-  name: 'AppHeader',
+  name: 'AppHero',
 };
 </script>
 
